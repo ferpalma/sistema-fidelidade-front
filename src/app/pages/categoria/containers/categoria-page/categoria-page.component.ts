1678e67d4c3d8753ce0caf801cde5752a259b643
Zhydani <zhydani@gmail.com>
2020-10-26 11:05:01 -0300

    ajustando algumas correcoes no projeto

--- src/app/pages/categoria/containers/categoria-page/categoria-page.component.ts
+++ src/app/pages/categoria/containers/categoria-page/categoria-page.component.ts
@@ -10,28 +10,13 @@ import { CategoriaService } from '../../../../service/categoria.service'
 })
 export class CategoriaPageComponent implements OnInit {
 
-  categoria: Categoria;
-  success: boolean = false;
-  errors: String[];
-  constructor(private service: CategoriaService) {
-      this.categoria = new Categoria();
+  constructor() {
+     
   }
 
   ngOnInit(): void {
   }
 
-  onSubmit() {
 
-      this.service.
-          salvar(this.categoria).
-          subscribe(response => {
-              this.success = true;
-              this.errors = null;
-          }, errorResponse => {
-              this.success = false;
-              this.errors = errorResponse.error.errors;
-          }
-       );
-  }
 
 }
