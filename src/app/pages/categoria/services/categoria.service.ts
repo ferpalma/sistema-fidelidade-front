b2f5678a576f5a9c16955e65653e54d2773bdf9b
zhydani <zhydani@gmail.com>
2020-10-30 23:23:29 -0300

    implementando crud para consumir o servico de categoria

--- src/app/pages/categoria/services/categoria.service.ts
+++ src/app/pages/categoria/services/categoria.service.ts
@@ -1,9 +1,79 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { retry, catchError } from 'rxjs/operators';
+import { Categoria } from '../models/categoria';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoriaService {
 
-  constructor() { }
+  url = 'http://localhost:3000/categorias'; // api rest fake
+
+  // injetando o HttpClient
+  constructor(private httpClient: HttpClient) { }
+
+  // Headers
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  }
+
+  // Obtem todas as categorias
+  getCategorias(): Observable<Categoria[]> {
+    return this.httpClient.get<Categoria[]>(this.url)
+      .pipe(
+        retry(2),
+        catchError(this.handleError))
+  }
+
+  // Obtem uma categoria pelo id
+  getCategoriaById(id: number): Observable<Categoria> {
+    return this.httpClient.get<Categoria>(this.url + '/' + id)
+      .pipe(
+        retry(2),
+        catchError(this.handleError)
+      )
+  }
+
+  // salva uma categoria
+  saveCar(categoria: Categoria): Observable<Categoria> {
+    return this.httpClient.post<Categoria>(this.url, JSON.stringify(categoria), this.httpOptions)
+      .pipe(
+        retry(2),
+        catchError(this.handleError)
+      )
+  }
+
+  // utualiza um carro
+  updateCar(categoria: Categoria): Observable<Categoria> {
+    return this.httpClient.put<Categoria>(this.url + '/' + categoria.id, JSON.stringify(categoria), this.httpOptions)
+      .pipe(
+        retry(1),
+        catchError(this.handleError)
+      )
+  }
+
+  // deleta uma categoria
+  deleteCar(categoria: Categoria) {
+    return this.httpClient.delete<Categoria>(this.url + '/' + categoria.id, this.httpOptions)
+      .pipe(
+        retry(1),
+        catchError(this.handleError)
+      )
+  }
+
+  // Manipulação de erros
+  handleError(error: HttpErrorResponse) {
+    let errorMessage = '';
+    if (error.error instanceof ErrorEvent) {
+      // Erro ocorreu no lado do client
+      errorMessage = error.error.message;
+    } else {
+      // Erro ocorreu no lado do servidor
+      errorMessage = `Código do erro: ${error.status}, ` + `menssagem: ${error.message}`;
+    }
+    console.log(errorMessage);
+    return throwError(errorMessage);
+  };
 }
