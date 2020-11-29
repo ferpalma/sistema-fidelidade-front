2abd76b59b80f153642bac15fdf0b514cc0c45b4
zhydani <zhydani@gmail.com>
2020-11-29 18:47:33 -0300

    corrigindo modulo

--- src/app/pages/pontuacao/pontuacao.module.ts
+++ src/app/pages/pontuacao/pontuacao.module.ts
@@ -3,12 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { PontuacaoRoutingModule } from './pontuacao-routing.module';
 import { PontuacaoPageComponent } from './containers/pontuacao-page/pontuacao-page.component';
-import { ListaPontuacaoComponent } from './components/lista-pontuacao/lista-pontuacao.component';
 import { CadastroPontuacaoComponent } from './components/cadastro-pontuacao/cadastro-pontuacao.component';
 
 
 @NgModule({
-  declarations: [PontuacaoPageComponent, ListaPontuacaoComponent, CadastroPontuacaoComponent],
+  declarations: [PontuacaoPageComponent, CadastroPontuacaoComponent],
   imports: [
     CommonModule,
     PontuacaoRoutingModule
