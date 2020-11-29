e84b53511375dead598250413436486053412253
zhydani <zhydani@gmail.com>
2020-11-29 18:37:33 -0300

    criando os modulos de promocao, pontuacao e resgate

---
+++ src/app/pages/pontuacao/pontuacao.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PontuacaoRoutingModule } from './pontuacao-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    PontuacaoRoutingModule
+  ]
+})
+export class PontuacaoModule { }
---
+++ src/app/pages/resgate/resgate.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ResgateRoutingModule } from './resgate-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ResgateRoutingModule
+  ]
+})
+export class ResgateModule { }
