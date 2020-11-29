fd776a9daf4a8d72f0af3fa9080380ec4a5f7765
zhydani <zhydani@gmail.com>
2020-11-29 18:40:54 -0300

    criando o componente de pontuacao

---
+++ src/app/pages/pontuacao/containers/pontuacao-page/pontuacao-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pontuacao-page',
+  templateUrl: './pontuacao-page.component.html',
+  styleUrls: ['./pontuacao-page.component.css']
+})
+export class PontuacaoPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/pages/pontuacao/pontuacao.module.ts
+++ src/app/pages/pontuacao/pontuacao.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PontuacaoRoutingModule } from './pontuacao-routing.module';
+import { PontuacaoPageComponent } from './containers/pontuacao-page/pontuacao-page.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [PontuacaoPageComponent],
   imports: [
     CommonModule,
     PontuacaoRoutingModule
