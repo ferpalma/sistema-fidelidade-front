163f10f9861148b8fdd02b9e1bf7e84d677b311a
zhydani <zhydani@gmail.com>
2020-11-29 18:51:11 -0300

    preparando a rota

--- src/app/pages/pontuacao/pontuacao-routing.module.ts
+++ src/app/pages/pontuacao/pontuacao-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PontuacaoPageComponent } from './containers';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: '',
+        component: PontuacaoPageComponent
+    }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
