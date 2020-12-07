ee6bf09f96fa543e752ae8433db5f257c511d811
victor152220 <victor.lustosa1522@live.com>
2020-12-07 00:54:41 -0300

    construindo um pouco de pontuacao de clientes

--- src/app/pages/pontuacao/components/lista-clientes-pontuacao/lista-clientes-pontuacao.component.ts
+++ src/app/pages/pontuacao/components/lista-clientes-pontuacao/lista-clientes-pontuacao.component.ts
@@ -1,5 +1,10 @@
+import { Cliente } from './../../../cliente/models/cliente';
 import { Component, OnInit } from '@angular/core';
-
+import { CadastroPontuacaoComponent } from '../cadastro-pontuacao/cadastro-pontuacao.component';
+import { catchError } from 'rxjs/operators';
+import { ClienteService } from '../../../cliente/services/cliente.service';
+import {MatDialog} from '@angular/material/dialog';
+import { empty, Observable, of } from 'rxjs';
 @Component({
   selector: 'app-lista-clientes-pontuacao',
   templateUrl: './lista-clientes-pontuacao.component.html',
@@ -7,9 +12,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListaClientesPontuacaoComponent implements OnInit {
 
-  constructor() { }
+  public listaClientes$: Observable<Cliente[]>;
+  public msgError: string;
 
   ngOnInit(): void {
+    this.msgError = null;
+    this.getListaClientes();
+  }
+  constructor(public dialog: MatDialog, private clienteService: ClienteService) {}
+    private getListaClientes() {
+    this.listaClientes$ = this.clienteService.getListaClientes()
+      .pipe(
+        catchError(error => {
+          this.msgError = error;
+          console.log("getListaClientes ListaClientesPontuacaoComponent : " + error);
+          // tslint:disable-next-line: deprecation
+          return empty();
+        })
+      );
+  }
+  public openDialog() {
+    this.dialog.open(CadastroPontuacaoComponent, {
+      width: '50%'
+    });
+    this.dialog.afterAllClosed.subscribe(() => {
+      this.getListaClientes();
+    });
   }
 
 }
