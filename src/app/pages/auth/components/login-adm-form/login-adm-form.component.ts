b665da356858779170e598e2b1ed7d17895183cd
victor152220 <victor.lustosa1522@live.com>
2020-12-20 23:19:58 -0300

    atualizando login adm

--- src/app/pages/auth/components/login-adm-form/login-adm-form.component.ts
+++ src/app/pages/auth/components/login-adm-form/login-adm-form.component.ts
@@ -1,5 +1,8 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { routes } from 'src/app/consts/routes';
+import { AuthService } from '../../services/auth.service';
 @Component({
   selector: 'app-login-adm-form',
   templateUrl: './login-adm-form.component.html',
@@ -9,8 +12,10 @@ export class LoginAdmFormComponent implements OnInit {
 
   @Output() sendLoginForm = new EventEmitter<void>();
   public form: FormGroup;
-
-
+  invalidLogin = false;
+  public routers: typeof routes = routes;
+  constructor(
+    private authService: AuthService, private router: Router) { }
   public ngOnInit(): void {
     this.form = new FormGroup({
       username: new FormControl('', [Validators.required]),
@@ -19,9 +24,19 @@ export class LoginAdmFormComponent implements OnInit {
       }
 
   public loginAdm(): void {
-    if (this.form.valid) {
-      this.sendLoginForm.emit();
-    }
+    (this.authService.authenticateFuncionario(this.form.get('username').value, this.form.get('password').value).subscribe(
+
+      data => {
+        this.router.navigate([this.routers.HOMEFUNCIONARIO]).then();
+        this.invalidLogin = false;
+      },
+      error => {
+        this.invalidLogin = true;
+
+      }
+    )
+    );
+
   }
 
 }
